328cb79318aeb0ca5f82a236d23c970ae8462326
arun32me <arun32me@gmail.com>
2019-05-05 21:13:04 +0530

    port changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 3000;
-const ip   = process.env.IP || 'localhost';
+const port = process.env.PORT || 8080;
+const ip   = process.env.IP || 0.0.0.0;
 const server = http.createServer(app);
 
 server.listen(port, ip, () => {
